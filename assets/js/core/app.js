e7085e1d9c6f24d261507247d0b2562c56e4a90e
saludHLG <capmicrohlg@gmail.com>
2026-08-22 22:36:55 -0400

    refactor: remove app legacy entrypoint

--- assets/js/core/app.js
+++ assets/js/core/app.js
@@ -1,5 +1,6 @@
-/* DatB app entrypoint. Legacy implementation kept intact while responsibilities are extracted. */
-document.write('<script src="assets/js/legacy/app_legacy.js"><\/script>');
+/* DatB app entrypoint. Shell y responsabilidades modulares. */
+document.write('<script src="assets/js/modules/app/shell.js"><\/script>');
+document.write('<script src="assets/js/modules/app/module_registry.js"><\/script>');
 document.write('<script src="assets/js/modules/app/navigation.js"><\/script>');
 document.write('<script src="assets/js/modules/app/navigation_render.js"><\/script>');
 document.write('<script src="assets/js/modules/app/module_router.js"><\/script>');
