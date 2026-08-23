6a0c9de53be233382ddc789da5230aecf67a91b7
saludHLG <capmicrohlg@gmail.com>
2026-08-22 21:58:02 -0400

    refactor(app): load navigation renderer

--- assets/js/core/app.js
+++ assets/js/core/app.js
@@ -1,3 +1,4 @@
 /* DatB app entrypoint. Legacy implementation kept intact while responsibilities are extracted. */
 document.write('<script src="assets/js/legacy/app_legacy.js"><\/script>');
 document.write('<script src="assets/js/modules/app/navigation.js"><\/script>');
+document.write('<script src="assets/js/modules/app/navigation_render.js"><\/script>');
