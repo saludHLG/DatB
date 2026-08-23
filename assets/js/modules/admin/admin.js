de297d9f8a0f819a0733b3458a92fb13318ca6f8
saludHLG <capmicrohlg@gmail.com>
2026-08-22 22:53:20 -0400

    refactor(admin): load shared context before modules

--- assets/js/modules/admin/admin.js
+++ assets/js/modules/admin/admin.js
@@ -1,4 +1,5 @@
-/* DatB admin entrypoint. Modular responsibilities plus runtime. */
+/* DatB admin entrypoint. Shared context first, then focused modules. */
+document.write('<script src="assets/js/modules/admin/admin_context.js"><\/script>');
 document.write('<script src="assets/js/modules/admin/admin_access.js"><\/script>');
 document.write('<script src="assets/js/modules/admin/admin_render.js"><\/script>');
 document.write('<script src="assets/js/modules/admin/admin_runtime.js"><\/script>');
