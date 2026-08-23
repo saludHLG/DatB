b85ba3cc35bce87603da7bc09fb21358cf8c148e
saludHLG <capmicrohlg@gmail.com>
2026-08-22 22:14:25 -0400

    refactor(app): load extracted digital signature module

--- assets/js/core/app.js
+++ assets/js/core/app.js
@@ -3,3 +3,4 @@ document.write('<script src="assets/js/legacy/app_legacy.js"><\/script>');
 document.write('<script src="assets/js/modules/app/navigation.js"><\/script>');
 document.write('<script src="assets/js/modules/app/navigation_render.js"><\/script>');
 document.write('<script src="assets/js/modules/app/module_router.js"><\/script>');
+document.write('<script src="assets/js/modules/app/signature.js"><\/script>');
