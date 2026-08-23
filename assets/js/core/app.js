98fcbdc0c8ef4421fd64d1fba06db371726c3002
saludHLG <capmicrohlg@gmail.com>
2026-08-22 21:57:07 -0400

    Wire extracted app navigation module

--- assets/js/core/app.js
+++ assets/js/core/app.js
@@ -1,2 +1,3 @@
 /* DatB app entrypoint. Legacy implementation kept intact while responsibilities are extracted. */
 document.write('<script src="assets/js/legacy/app_legacy.js"><\/script>');
+document.write('<script src="assets/js/modules/app/navigation.js"><\/script>');
