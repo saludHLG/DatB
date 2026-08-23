14acf9b5249a191a924c1de35d74086f29845e6d
saludHLG <capmicrohlg@gmail.com>
2026-08-22 22:15:33 -0400

    refactor(app): load profile module

--- assets/js/core/app.js
+++ assets/js/core/app.js
@@ -4,3 +4,4 @@ document.write('<script src="assets/js/modules/app/navigation.js"><\/script>');
 document.write('<script src="assets/js/modules/app/navigation_render.js"><\/script>');
 document.write('<script src="assets/js/modules/app/module_router.js"><\/script>');
 document.write('<script src="assets/js/modules/app/signature.js"><\/script>');
+document.write('<script src="assets/js/modules/app/profile.js"><\/script>');
