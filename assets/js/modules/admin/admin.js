5c9359ad42068d1a5cf5f8af762308173a9568a7
saludHLG <capmicrohlg@gmail.com>
2026-08-22 22:35:40 -0400

    refactor(admin): wire render module into entrypoint

--- assets/js/modules/admin/admin.js
+++ assets/js/modules/admin/admin.js
@@ -1,3 +1,4 @@
 /* DatB admin entrypoint. Modular responsibilities are loaded before legacy behavior. */
 document.write('<script src="assets/js/modules/admin/admin_access.js"><\/script>');
+document.write('<script src="assets/js/modules/admin/admin_render.js"><\/script>');
 document.write('<script src="assets/js/legacy/admin_legacy.js"><\/script>');
